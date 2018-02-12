9a4bfd981c1b5e040991aded5f3c60842e34aba9
Erin Walker <erinb0317@hotmail.com>
2018-02-12 00:38:59 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,20 +11,21 @@ class Triangle
     case
     when side1 <=0 || side2 <= 0 || side3 <=0
       raise TriangleError
-    when (side1 + side2) <= side3 || (side2 + side3) <= side1 || (side3 + side1) <= side1 
+    when (side1 + side2) <= side3 || (side2 + side3) <= side1 || (side3 + side1) <= side1
       raise TriangleError
-    when side1 == side2 && side2 == side3 
+    when side1 == side2 && side2 == side3
       :equilateral
-    when side1 == side2 || side1 == side3 || side2 == side3 
+    when side1 == side2 || side1 == side3 || side2 == side3
       :isosceles
     else
       :scalene
     end
-end
+  end
 
 class TriangleError < StandardError
   def message
     "Please input the lengths of a triangle."
   end
+end
 
 end
