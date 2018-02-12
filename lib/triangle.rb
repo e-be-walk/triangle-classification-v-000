4b5e664adc1fc757ac039b50a25310574153ef2c
Erin Walker <erinb0317@hotmail.com>
2018-02-12 00:33:04 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind 
+    case 
+    when 
+      
+    end
+end
+
+class TriangleError < StandardError
+  def message
+    "Please input the lengths of a triangle."
+  end
+
 end
